d685ad5a28870df37272ca51ccfaefaf3b980d0b
assignment <bmwthriad2023@gmail.com>
2026-03-11 06:12:01 +0600

    feat(auth): implement signup method in AuthRepository

--- lib/features/auth/data/repositories/auth_repository.dart
+++ lib/features/auth/data/repositories/auth_repository.dart
@@ -1,23 +1,41 @@
 // lib/features/auth/data/repositories/auth_repository.dart
 import 'package:dio/dio.dart';
+import 'package:flutter_instabuy_app/features/auth/data/models/sign_up_response.dart';
 import '../../../../core/api/api_constants.dart';
 import '../../../../core/api/dio_client.dart';
 import '../models/login_response.dart';
+
 
 class AuthRepository {
   final DioClient _client = DioClient();
 
   Future<LoginResponse> login(String email, String password) async {
-    try {
-      final response = await _client.dio.post(
-        ApiConstants.loginEndpoint,
-        data: {"email": email, "password": password},
-      );
+    final response = await _client.dio.post(
+      ApiConstants.loginEndpoint,
+      data: {"email": email, "password": password},
+    );
+    return LoginResponse.fromJson(response.data);
+  }
 
-      return LoginResponse.fromJson(response.data);
-      // Here response.data = full json which I have seen in postman
-    } on DioException catch (e) {
-      throw Exception("Login failed: ${e.response?.data ?? e.message}");
-    }
+  Future<SignupResponse> signup({
+    required String firstName,
+    required String lastName,
+    required String email,
+    required String password,
+    required String phone,
+    required String city,
+  }) async {
+    final response = await _client.dio.post(
+      ApiConstants.signupEndpoint,
+      data: {
+        "first_name": firstName,
+        "last_name": lastName,
+        "email": email,
+        "password": password,
+        "phone": phone,
+        "city": city,
+      },
+    );
+    return SignupResponse.fromJson(response.data);
   }
 }
