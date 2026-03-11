c41df825f56fe750745cedc171a2a6776abadaf8
assignment <bmwthriad2023@gmail.com>
2026-03-11 06:14:41 +0600

    feat(auth): add signup API endpoint constant in ApiConstants

--- lib/core/api/api_constants.dart
+++ lib/core/api/api_constants.dart
@@ -2,4 +2,5 @@
 class ApiConstants {
   static const String baseUrl = "https://ecom-rs8e.onrender.com/api";
   static const String loginEndpoint = "$baseUrl/auth/login";
+  static const String signupEndpoint = "$baseUrl/auth/signup";
 }
