f174d1dd8d4205083df30e66b1bd4731eba5a351
assignment <bmwthriad2023@gmail.com>
2026-03-12 21:38:28 +0600

    chore (core): added slides endpoint

--- lib/core/api/api_constants.dart
+++ lib/core/api/api_constants.dart
@@ -3,4 +3,5 @@ class ApiConstants {
   static const String baseUrl = "https://ecom-rs8e.onrender.com/api";
   static const String loginEndpoint = "$baseUrl/auth/login";
   static const String signupEndpoint = "$baseUrl/auth/signup";
+  static const String slidesEndpoint = "$baseUrl/slides";
 }
